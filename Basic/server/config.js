9092c62d6e96fd8f2ff3e10acaf0e3f775af759d
Luodina <marta.rudina@gmail.com>
2018-04-04 15:58:03 +0800

    new favicon

--- Basic/server/config.js
+++ Basic/server/config.js
@@ -3,7 +3,7 @@ module.exports = {
     dev: {
         dist: 'app',
         port: 9000,
-        mysql: 'mysql://root:777777@127.0.0.1:3306/ocai',
+        mysql: 'mysql://root:777777@127.0.0.1:3306/Sensetime',
     },
     prod: {},
     env: 'dev',
